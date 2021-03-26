0a59039e12539c34e0a2ec4f2a792c096dea485c
Tanay Menezes <tm452@cornell.edu>
2021-03-25 20:14:07 -0400

    created skeleton of Person module, to be debugged

---
+++ person.ml
@@ -0,0 +1,42 @@
+type t = {
+  player_board : Battleship.board;
+  oppponent_board : Battleship.board;
+}
+
+type action = 
+    | Place of (Battleship.ship_type, Battleship.block_tile)
+    | Attack of Battleship.block_tile
+    | Quit
+
+(** Raised when an empty command is parsed. *)
+exception Empty
+
+(** Raised when a malformed command is encountered. *)
+exception Malformed
+
+let rec remove_empty (lst : string list) =
+  match lst with
+  | [] -> raise Empty
+  | h :: t ->
+      if String.equal "" h then remove_empty_strings t
+      else h :: remove_empty_strings t
+
+let create_place_command lst = 
+  match lst with
+  | 
+
+let create_attack_command lst = 
+  match lst with
+  |
+
+let parse_input input =
+  let words = String.split_on_char ' ' input in
+  let no_empty = remove_empty words in
+  match no_empty with
+  | [] -> raise Empty
+  | h :: t -> (
+      match h with
+      | "place" -> create_place_command t
+      | "attack" -> create_attack_command t
+      | "quit" -> Quit
+      | _ -> raise Malformed)
---
+++ person.mli
@@ -0,0 +1,14 @@
+type t
+
+type action = 
+    | Place of (Battleship.ship_type, Battleship.block_tile)
+    | Attack of Battleship.block_tile
+    | Quit
+
+(** Raised when an empty command is parsed. *)
+exception Empty
+
+(** Raised when a malformed command is encountered. *)
+exception Malformed
+
+val parse_input : string -> action
