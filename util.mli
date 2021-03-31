acf097dbdfacd7f3e612a7e0c296db56ca2f988b
William Barkoff <william@barkoffusa.com>
2021-03-31 18:21:18 -0400

    update documentation comments

--- util.mli
+++ util.mli
@@ -3,9 +3,9 @@
 
     [pretty_string_of_list \["item"\]] returns ["item"].
 
-    [pretty_string_of_list \["apples"; "bananas"]] returns ["apples and
-    bananas"]
+    [pretty_string_of_list \["apples"; "bananas"\]] returns
+    ["apples and bananas"]
 
-    [pretty_string_of_list ["apples"; "oranges"; "grapes"; "bananas"]]
-    returns ["apples, oranges, grapes, and bananas"]*)
+    [pretty_string_of_list \["apples"; "oranges"; "grapes"; "bananas"\]]
+    returns ["apples, oranges, grapes, and bananas"] *)
 val pretty_string_of_list : string list -> string
