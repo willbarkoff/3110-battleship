9c037060a5886a6fd300f573e0b4c0bd638cce9b
Travis Zhang <tz98@cornell.edu>
2021-04-22 16:52:31 -0400

    Removed listener from parameters of function

--- bin/main.ml
+++ bin/main.ml
@@ -143,5 +143,4 @@ let _ =
   let speclist = [ ("-l", Arg.Set local, "Play locally") ] in
 
   Arg.parse speclist (fun _ -> ()) usage_message;
-  if !local then show_main_menu ()
-  else Network.listen_and_serve 1234 (fun _ -> failwith "TODO")
+  if !local then show_main_menu () else Network.listen_and_serve 1234
