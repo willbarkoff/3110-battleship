b5411969fce30eee812116a99dc0c0d5f7130b26
William Barkoff <william@barkoffusa.com>
2021-04-30 00:35:33 -0400

    delete unused network
    cherry picked from d50b0ab64abcd9a089cf33ead9d75418c9e0dde5

--- lib/network/network.mli
+++
@@ -1,8 +0,0 @@
-(** Network holds networking related features for the battleship game. *)
-
-(** [message] represents the type of message sent to or from the server.*)
-type message
-
-(** [listen_and_serve p l] is a function that listens on port [p] and
-    handles messages with the [listener] [l].*)
-val listen_and_serve : int -> unit
--- lib/network/server.mli
+++
@@ -1,3 +0,0 @@
-type server_func = in_channel -> out_channel -> unit
-
-val establish_vmem_server : server_func -> Unix.sockaddr -> unit
