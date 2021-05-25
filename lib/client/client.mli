a69f1df6f7cab92c8fb9ab2ed1288df3f78900d3
William Barkoff <william@barkoffusa.com>
2021-05-24 23:38:01 -0400

    Add documentation for `Client` module

--- lib/client/client.mli
+++ lib/client/client.mli
@@ -1,3 +1,9 @@
+(** The Client module provides a client for playing a game over the
+    network. It differs from the Server module in that it does not
+    handle any of the game logic or scoring, it simply provides a means
+    to interact with the server.*)
+
+(** [message] represents a message sent over the network. *)
 type message
 
 (** [play_internet_game addr] plays a game over the network with the
