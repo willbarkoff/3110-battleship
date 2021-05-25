4a5c79352e302846b4e3ca1ae7b096341248f399
William Barkoff <william@barkoffusa.com>
2021-05-24 22:32:00 -0400

    FAILING: hashmap for networking

--- lib/server/server.ml
+++ lib/server/server.ml
@@ -2,7 +2,35 @@ exception Invalid
 
 type game = out_channel list
 
+let _ = Random.self_init ()
+
+(** The expected load is the number of expected concurrent games,
+    initally. It is so that we know the size that the [games] Hashtbl
+    should start at. *)
+let expected_load = 10
+
+(** The length of a gamecode *)
+let gamecode_size = 6
+
+(** The alphabet to be used when generating gamecodes. It's a list of
+    strings rather than a list of characters, which means you could do
+    some cool things with this, like make each one an animal or
+    something. If only I were creative.*)
+let gamecode_alphabet =
+  Util.explode
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+  |> List.map (fun chr -> String.make 1 chr)
+
+let generate_gamecode () =
+  List.init gamecode_size (fun _ ->
+      Random.int (List.length gamecode_alphabet))
+  |> List.fold_left
+       (fun acc num -> List.nth gamecode_alphabet num ^ acc)
+       ""
+
 let new_game p1 = [ p1 ]
+
+let games = Hashtbl.create expected_load
 
 type message =
   | PassState of State.t
@@ -10,7 +38,7 @@ type message =
 
 type communication_message =
   | BroadcastState of int * State.t
-  | AddMeToGame of out_channel
+  | AddMeToGame of string * out_channel
   | CMNullMessage
 
 type main_message =
@@ -43,34 +71,41 @@ let write_message chan (msg : message) =
   Marshal.to_channel chan msg [];
   flush chan
 
+let check_intern_chan players =
+  match
+    Event.receive internal_chan
+    |> Event.poll
+    |> Option.value ~default:CMNullMessage
+  with
+  | BroadcastState (whom, what) ->
+      PassState what |> write_message (List.nth !players whom)
+  | AddMeToGame (code, chan) ->
+      Hashtbl.replace games code (Hashtbl.find games code @ [ chan ])
+  | _ -> ()
+
+let check_main_chan players =
+  match
+    Event.receive main_chan |> Event.poll
+    |> Option.value ~default:MMNullMessage
+  with
+  | GetNumPlayers ->
+      NumPlayers (List.length !players)
+      |> Event.send main_chan |> Event.sync
+  | _ -> ()
+
 let spawn_communication_manager () =
   print_endline "CREATED THREAD";
   Thread.create
     (fun _ ->
       let players = ref [] in
       while true do
-        (match
-           Event.receive internal_chan
-           |> Event.poll
-           |> Option.value ~default:CMNullMessage
-         with
-        | BroadcastState (whom, what) ->
-            PassState what |> write_message (List.nth !players whom)
-        | AddMeToGame c -> players := !players @ [ c ]
-        | _ -> ());
-        match
-          Event.receive main_chan |> Event.poll
-          |> Option.value ~default:MMNullMessage
-        with
-        | GetNumPlayers ->
-            NumPlayers (List.length !players)
-            |> Event.send main_chan |> Event.sync
-        | _ -> ()
+        check_intern_chan players;
+        check_main_chan players
       done)
     ()
   |> ignore
 
-let rec handler player_num input output =
+let rec handler player_num input output code =
   try
     let msg = input |> read_message in
     print_endline "GOT MESSAGE";
@@ -82,7 +117,7 @@ let rec handler player_num input output =
           |> Event.sync
       | _ -> Error "Invalid message" |> write_message output
     end;
-    handler player_num input output
+    handler player_num input output code
   with
   | End_of_file ->
       write_message output (Error "EOF")
@@ -96,14 +131,20 @@ let setup_handler (input : in_channel) (output : out_channel) =
   match Event.receive main_chan |> Event.sync with
   | NumPlayers n ->
       if n = 0 then begin
-        AddMeToGame output |> Event.send internal_chan |> Event.sync;
+        let code = generate_gamecode () in
+        AddMeToGame (code, output)
+        |> Event.send internal_chan
+        |> Event.sync;
         print_endline "PLAYER 0 JOINED";
-        handler 0 input output
+        handler 0 input output code
       end
       else if n = 1 then begin
-        AddMeToGame output |> Event.send internal_chan |> Event.sync;
+        let gc = "5" in
+        AddMeToGame (gc, output)
+        |> Event.send internal_chan
+        |> Event.sync;
         print_endline "PLAYER 1 JOINED";
-        handler 1 input output
+        handler 1 input output gc
       end
       else Error "The game is full" |> write_message output
   | _ ->
